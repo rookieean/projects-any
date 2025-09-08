dfb920defa8081d692a85410e9033098d1bfad82
arena-belajar <septaranja@gmail.com>
2025-09-08 08:37:54 +0700

    DOM dalam js

---
+++ nyoba-js/#10-DOM.js
@@ -0,0 +1,14 @@
+// Dom document object model
+// is kind of document for code, there's rule for it
+
+
+// example in js
+const paragraphs = document.querySelectorAll('p');
+// paragraphs[0] is the first <p> element
+
+alert(paragraphs[0].nodeName);
+
+// when web page downloading, browser automatic make a DOM page
+// the object doc access with window.document ar just document
+
+
---
+++ nyoba-js/DOM-praktik/branch.js
@@ -0,0 +1,17 @@
+// Example 
+{
+  const takeContohText = document.getElementById("contoh").innerHTML
+  
+   document.getElementById("example").innerText = takeContohText
+}
+// expected result = Example : Buatlah id=contoh ini menjadi innerText pada id=example
+// End of Example
+//Case 1
+  // write your code here
+  // note : perlu diingiat jika ingin mengambil DOM menggunakan className harus melakukan sesuai index
+  const takeCaseText = {
+        
+        
+ // expected result = Case 1 : Buatlah class=caseText ini menjadi innerText pada id=Case1
+// End of Case 1
+}
